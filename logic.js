732cf3330208e318fb76da92369c8b618cfde863
Harish Swaminathan <96258818+venamvalikudhu@users.noreply.github.com>
2023-08-15 10:49:08 +0530

    Update logic.js

--- logic.js
+++ logic.js
@@ -17,31 +17,29 @@ var oneMinusNumber = 0;
 function shuffleForNextNumber()
 {
 clearInterval(timerInterval);
-
+document.getElementById("shuffle").disabled= true;
 document.getElementById("last-number").innerText = oneMinusNumber;
 document.getElementById("plane-loader-area").animate(plane_loader_anim,plane_loader);
 
+do
+{	
 nextNumber = Math.floor(Math.random() * 100);
-if(!numberPicked.includes(nextNumber) && nextNumber!=0) 
-	{
-    numberPicked.push(nextNumber);
-    }
-else
-{
-	do
-	{
-	nextNumber = Math.floor(Math.random() * 100);
-	}while(numberPicked.includes(nextNumber) && nextNumber!=0)
-    numberPicked.push(nextNumber);			
-}
+}while(numberPicked.includes(nextNumber) || nextNumber==0)
+numberPicked.push(nextNumber);			
 
 var text = "count"+nextNumber;
  timerInterval = setInterval(() => {
    document.getElementById("current-value").innerText = nextNumber;
-   document.getElementById(text).style.border = '2px dotted yellow';
-   document.getElementById(text).style.color = 'yellow';	 
+   document.getElementById(text).style.backgroundColor='lightgreen'
+   document.getElementById(text).style.borderColor='black'
+   document.getElementById(text).style.color = 'black';	
+   document.getElementById("sequence-list").innerText = numberPicked;
+   document.getElementById("shuffle").disabled= false;
+   if(numberPicked.length==99)
+	{
+    document.getElementById('score-modal').style.display = "flex"
+	}
 },3000);
-
 oneMinusNumber = nextNumber;
 
 }
